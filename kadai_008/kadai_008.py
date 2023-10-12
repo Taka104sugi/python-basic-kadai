32a22907d00e106c7f542ae880c7386125d9c828
Taka104sugi <145530668+Taka104sugi@users.noreply.github.com>
2023-10-12 13:42:35 +0900

    Add files via upload

---
+++ kadai_008/kadai_008.py
@@ -0,0 +1,10 @@
+var = int(input("正の整数を入力してください: "))
+
+elif var % 3 == 0:
+    print("Fizz")
+elif var % 5 == 0:
+    print("Buzz")
+if var % 3 == 0 and var % 5 == 0:
+    print("FizzBuzz")
+else:
+    print(var)
